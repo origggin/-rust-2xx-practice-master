b6b5a507954a06e281780766e167f41df9317194
origggin <max.lavrinenko@e-u.edu.ua>
2024-10-05 14:45:30 +0300

    Completed Test 4.1 and make template for features tasks
    Completed one task and made a template for completing others

--- src/chapter4.rs
+++ src/chapter4.rs
@@ -1,5 +1,61 @@
 /// https://practice.course.rs/basic-types/numbers.html
 #[test]
-fn test41() {
+fn test11() {
+        let x: i32 = 5;
+        let mut y: i32 = 5;
 
+        y = x;
+
+        let z = 10; // Type of z ?
+
+        println!("Success!");
+}
+
+#[test]
+fn test12() {
+
+}
+
+#[test]
+fn test13() {
+
+}
+#[test]
+fn test14() {
+
+}
+
+#[test]
+fn test15() {
+
+}
+
+#[test]
+fn test16() {
+
+}
+
+#[test]
+fn test17() {
+
+}
+
+#[test]
+fn test18() {
+
+}
+
+#[test]
+fn test19() {
+
+}
+
+#[test]
+fn test110() {
+
+}
+
+#[test]
+fn test111() {
+    
 }
