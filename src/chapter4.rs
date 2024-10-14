5b4ccecfcefacfef2d34bd8b5db1795606b743e5
Max.Lavrinenko <max.lavrinenko@e-u.edu.ua>
2024-10-14 19:58:45 +0300

    Finish Chapter 4 from laptop saves

--- src/chapter4.rs
+++ src/chapter4.rs
@@ -276,25 +276,92 @@ fn sum(x: i32, y: i32) -> i32 {
 
 #[test]
 fn test41() {
-    // Пример 1 из 4.4
+    fn main() {
+    let (x, y) = (1, 2);
+    let s = sum(x, y);
+
+    assert_eq!(s, 3);
+
+    println!("Success!");
+}
+
+fn sum(x: i32, y: i32) -> i32 { 
+    x + y 
+}
+
 }
 
 #[test]
 fn test42() {
-    // Пример 2 из 4.4
+    fn main() {
+    print();
+}
+fn print() -> () {
+    println!("Success!");
+}
+
 }
 
 #[test]
 fn test43() {
-    // Пример 3 из 4.4
+    fn main() {
+    never_return();
+
+    println!("Failed!");
+}
+
+fn never_return() -> ! {
+    loop {} // Infinite loop
+}
+
+        /// или 
+fn main() {
+    never_return();
+
+    println!("Failed!");
+}
+
+fn never_return() -> ! {
+    panic!("This function will never return!");
+}
+
 }
 
 #[test]
 fn test44() {
-    // Пример 4 из 4.4
+    fn main() {
+    println!("Success!");
+}
+
+fn get_option(tp: u8) -> Option<i32> {
+    match tp {
+        1 => {
+            Some(42) 
+        }
+        _ => {
+            never_return_fn()
+        }
+    }
+}
+
+fn never_return_fn() -> ! {
+    loop {}
+}
 }
 
 #[test]
 fn test45() {
-    // Пример 5 из 4.4
+    fn main() {
+    let b = true; 
+    let _v = match b {
+        true => 1,
+        false => {
+            println!("Success!");
+            panic!("we have no value for `false`, but we can panic");
+        }
+    };
+
+    println!("Exercise Failed if printing out this line!");
+}
+
 }
