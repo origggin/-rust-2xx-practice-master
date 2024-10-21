b2c368f55d68ff23e3470f2fdbbbc9cc7317ec02
Max.Lavrinenko <max.lavrinenko@e-u.edu.ua>
2024-10-21 19:37:43 +0300

    Update chapter14.rs

--- src/chapter14.rs
+++ src/chapter14.rs
@@ -1,1 +1,70 @@
-// Content for chapter 14 
+//Yak zhe ya zadovbavsya
+#[test]
+fn test1_1() {
+
+}
+
+#[test]
+fn test1_2() {
+
+}
+
+#[test]
+fn test1_3() {
+
+}
+
+#[test]
+fn test1_4() {
+
+}
+
+#[test]
+fn test1_5() {
+
+}
+
+#[test]
+fn test1_6() {
+
+}
+
+#[test]
+fn test2_1() {
+
+}
+
+#[test]
+fn test2_2() {
+
+}
+
+#[test]
+fn test2_3() {
+
+}
+
+#[test]
+fn test2_4() {
+
+}
+
+#[test]
+fn test2_5() {
+
+}
+
+#[test]
+fn test3_1() {
+
+}
+
+#[test]
+fn test3_2() {
+
+}
+
+#[test]
+fn test3_3() {
+
+}
