6138a81a8fc8c709189d96c82c89ba873c73a5e0
Max.Lavrinenko <max.lavrinenko@e-u.edu.ua>
2024-10-21 18:52:28 +0300

    Update chapter5.rs

--- src/chapter5.rs
+++ src/chapter5.rs
@@ -110,55 +110,133 @@ fn test19() {
 
 #[test]
 fn test21() {
-    // Код теста для 2.1
-}
+fn main() {
+    let x = 5;
+    // fill the blank
+    let p = &x;
+ 
+    println!("the memory address of x is {:p}", p); // one possible output: 0x16fa3ac84
+}}
 
 #[test]
 fn test22() {
-    // Код теста для 2.2
-}
+fn main() {
+    let x = 5;
+    let y = &x;
+
+    // modify this line only
+    assert_eq!(5, *y);
+}}
 
 #[test]
 fn test23() {
-    // Код теста для 2.3
+fn main() {
+    let mut s = String::from("hello, ");
+
+    borrow_object(&s)
 }
+
+fn borrow_object(s: &String) {}}
 
 #[test]
 fn test24() {
-    // Код теста для 2.4
+fn main() {
+    let mut s = String::from("hello, ");
+
+    push_str(&mut s)
 }
+
+fn push_str(s: &mut String) {
+    s.push_str("world")
+}}
 
 #[test]
 fn test25() {
-    // Код теста для 2.5
-}
+fn main() {
+    let mut s = String::from("hello, ");
+
+    // fill the blank to make it work
+    let p = &mut s;
+    
+    p.push_str("world");
+}}
 
 #[test]
 fn test26() {
-    // Код теста для 2.6
+fn main() {
+    let c = '中';
+
+    let r1 = &c;
+    // fill the blank，dont change other code
+    let ref r2 = c;
+
+    assert_eq!(*r1, *r2);
+    
+    // check the equality of the two address strings
+    assert_eq!(get_addr(r1),get_addr(r2));
 }
+
+// get memory address string
+fn get_addr(r: &char) -> String {
+    format!("{:p}", r)
+}}
 
 #[test]
 fn test27() {
-    // Код теста для 2.7
-}
+fn main() {
+    let s = String::from("hello");
+
+    let r1 = &s;
+    let r2 = &s;
+
+    println!("{}, {}", r1, r2);
+}}
 
 #[test]
 fn test28() {
-    // Код теста для 2.8
+fn main() {
+    //fix error by modifying this line
+    let mut s = String::from("hello, ");
+
+    borrow_object(&mut s)
 }
+
+fn borrow_object(s: &mut String) {}}
 
 #[test]
 fn test29() {
-    // Код теста для 2.9
+fn main() {
+    let mut s = String::from("hello, ");
+
+    borrow_object(&s);
+    
+    s.push_str("world");
 }
+
+fn borrow_object(s: &String) {}}
 
 #[test]
 fn test210() {
-    // Код теста для 2.10
-}
+fn main() {
+    let mut s = String::from("hello, ");
+
+    let r1 = &mut s;
+    r1.push_str("world");
+    let r2 = &mut s;
+    r2.push_str("!");
+    
+    // println!("{}",r1);
+}}
 
 #[test]
 fn test211() {
-    // Код теста для 2.11
-}
+fn main() {
+    let mut s = String::from("hello, ");
+
+    let r1 = &mut s;
+    let r2 = &mut s;
+
+    // add one line below to make a compiler error: cannot borrow `s` as mutable more than once at a time
+    // you can't use r1 and r2 at the same time
+    r1.push_str("world");
+}}
