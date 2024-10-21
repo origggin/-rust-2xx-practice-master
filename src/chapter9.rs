339a75ba72ea42655b42d52f041d72abff005b92
Max.Lavrinenko <max.lavrinenko@e-u.edu.ua>
2024-10-21 19:11:34 +0300

    Create chapter9.rs

---
+++ src/chapter9.rs
@@ -0,0 +1,29 @@
+#[test]
+fn test1_1() {
+
+}
+
+#[test]
+fn test1_2() {
+
+}
+
+#[test]
+fn test1_3() {
+
+}
+
+#[test]
+fn test1_4() {
+
+}
+
+#[test]
+fn test1_5() {
+
+}
+
+#[test]
+fn test1_6() {
+
+}
