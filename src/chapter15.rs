14c6578acc446c69b5e0c2cc4ba567c7fb1403cc
Max.Lavrinenko <max.lavrinenko@e-u.edu.ua>
2024-10-21 19:44:26 +0300

    Update chapter15.rs

--- src/chapter15.rs
+++ src/chapter15.rs
@@ -1,1 +1,25 @@
-// Content for chapter 15 
+#[test]
+fn test1_1() {
+fn main() {
+    // todo!();
+    // unimplemented!();
+
+    assert_eq!(6, 5 + 3 + 2 + 1);
+}
+
+}
+
+#[test]
+fn test1_2() {
+
+}
+
+#[test]
+fn test1_3() {
+
+}
+
+#[test]
+fn test1_4() {
+
+}
