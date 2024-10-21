b5e0b12f55aa41489348ec7866c0629730e73d32
Max.Lavrinenko <max.lavrinenko@e-u.edu.ua>
2024-10-21 19:35:55 +0300

    Update chapter13.rs

--- src/chapter13.rs
+++ src/chapter13.rs
@@ -1,1 +1,228 @@
-// Content for chapter 13 
+#[test]
+fn test1_1() {
+use core::panic;
+
+fn drink(beverage: &str) {
+    if beverage == "lemonade" {
+        println!("Success!");
+        // IMPLEMENT the below code
+        panic!("drinked, duang.....peng!")
+     }
+
+    println!("Exercise Failed if printing out this line!");
+}
+
+fn main() {
+    drink("lemonade");
+
+    println!("Exercise Failed if printing out this line!");
+}
+}
+
+#[test]
+fn test1_2() {
+// MAKE the code work by fixing all panics
+fn main() {
+    assert_eq!("abc".as_bytes(), [97, 98, 99]);
+
+    let v = vec![1, 2, 3];
+    let ele = v[2];
+    // unwrap may panic when get return a None
+    let ele = v.get(2).unwrap();
+
+    // Sometimes, the compiler is unable to find the overflow errors for you in compile time ,so a panic will occur
+     let v = production_rate_per_hour(2);
+
+     // because of the same reason as above, we have to wrap it in a function to make the panic occur
+     divide(15, 1);
+
+     println!("Success!")
+}
+
+fn divide(x:u8, y:u8) {
+    println!("{}", x / y)
+}
+
+fn production_rate_per_hour(speed: u8) -> f64 {
+    let cph: u8 = 21;
+    match speed {
+        1..=4 => (speed * cph) as f64,
+        5..=8 => (speed * cph) as f64 * 0.9,
+        9..=10 => (speed * cph) as f64 * 0.77,
+        _ => 0 as f64,
+    }
+}
+
+pub fn working_items_per_minute(speed: u8) -> u32 {
+    (production_rate_per_hour(speed) / 60 as f64) as u32
+}
+}
+
+#[test]
+fn test1_3() {
+
+}
+
+#[test]
+fn test2_1() {
+use std::num::ParseIntError;
+
+fn multiply(n1_str: &str, n2_str: &str) -> Result<i32, ParseIntError> {
+    let n1 = n1_str.parse::<i32>();
+    let n2 = n2_str.parse::<i32>();
+    Ok(n1.unwrap() * n2.unwrap())
+}
+
+fn main() {
+    let result = multiply("10", "2");
+    assert_eq!(result, Ok(20));
+
+    let result = multiply("4", "2");
+    assert_eq!(result.unwrap(), 8);
+
+    println!("Success!")
+}
+}
+
+#[test]
+fn test2_2() {
+use std::num::ParseIntError;
+
+// IMPLEMENT multiply with ?
+// DON'T use unwrap here
+fn multiply(n1_str: &str, n2_str: &str) -> Result<i32, ParseIntError> {
+    let n1 = n1_str.parse::<i32>()?;
+    let n2 = n2_str.parse::<i32>()?;
+    Ok(n1 * n2)
+}
+
+fn main() {
+    assert_eq!(multiply("3", "4").unwrap(), 12);
+    println!("Success!")
+}
+}
+
+#[test]
+fn test2_3() {
+use std::fs::File;
+use std::io::{self, Read};
+
+fn read_file1() -> Result<String, io::Error> {
+    let f = File::open("hello.txt");
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+    
+    let mut s = String::new();
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
+}
+
+fn read_file2() -> Result<String, io::Error> {
+    let mut s = String::new();
+
+    File::open("hello.txt")?.read_to_string(&mut s)?;
+
+    Ok(s)
+}
+
+fn main() {
+    assert_eq!(read_file1().unwrap_err().to_string(), read_file2().unwrap_err().to_string());
+    println!("Success!")
+}
+}
+
+#[test]
+fn test2_4() {
+use std::num::ParseIntError;
+
+fn add_two(n_str: &str) -> Result<i32, ParseIntError> {
+   n_str.parse::<i32>().map(|num| num +2)
+}
+
+fn main() {
+    assert_eq!(add_two("4").unwrap(), 6);
+
+    println!("Success!")
+}
+}
+
+#[test]
+fn test2_5() {
+use std::num::ParseIntError;
+
+// With the return type rewritten, we use pattern matching without `unwrap()`.
+// But it's so Verbose..
+fn multiply(n1_str: &str, n2_str: &str) -> Result<i32, ParseIntError> {
+    match n1_str.parse::<i32>() {
+        Ok(n1)  => {
+            match n2_str.parse::<i32>() {
+                Ok(n2)  => {
+                    Ok(n1 * n2)
+                },
+                Err(e) => Err(e),
+            }
+        },
+        Err(e) => Err(e),
+    }
+}
+
+// Rewriting `multiply` to make it succinct
+// You  MUST USING `and_then` and `map` here
+fn multiply1(n1_str: &str, n2_str: &str) -> Result<i32, ParseIntError> {
+    // IMPLEMENT...
+    n1_str.parse::<i32>().and_then(|n1| {
+        n2_str.parse::<i32>().map(|n2| n1 * n2)
+    })
+}
+
+fn print(result: Result<i32, ParseIntError>) {
+    match result {
+        Ok(n)  => println!("n is {}", n),
+        Err(e) => println!("Error: {}", e),
+    }
+}
+
+fn main() {
+    // This still presents a reasonable answer.
+    let twenty = multiply1("10", "2");
+    print(twenty);
+
+    // The following now provides a much more helpful error message.
+    let tt = multiply("t", "2");
+    print(tt);
+
+    println!("Success!")
+}
+}
+
+#[test]
+fn test2_6() {
+use std::num::ParseIntError;
+
+// Define a generic alias for a `Result` with the error type `ParseIntError`.
+type Res<T> = Result<T, ParseIntError>;
+
+// Use the above alias to refer to our specific `Result` type.
+fn multiply(first_number_str: &str, second_number_str: &str) -> Res<i32> {
+    first_number_str.parse::<i32>().and_then(|first_number| {
+        second_number_str.parse::<i32>().map(|second_number| first_number * second_number)
+    })
+}
+
+// Here, the alias again allows us to save some space.
+fn print(result: Res<i32>) {
+    match result {
+        Ok(n)  => println!("n is {}", n),
+        Err(e) => println!("Error: {}", e),
+    }
+}
+
+fn main() {
+    print(multiply("10", "2"));
+    print(multiply("t", "2"));
+}
+}
