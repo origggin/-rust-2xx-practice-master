79a0b1e2ec6f6f8354ad558de13859cceea30526
Max.Lavrinenko <max.lavrinenko@e-u.edu.ua>
2024-10-21 19:14:36 +0300

    Create chapter10.rs

---
+++ src/chapter10.rs
@@ -0,0 +1,144 @@
+#[test]
+fn test1_1() {
+
+}
+
+#[test]
+fn test1_2() {
+
+}
+
+#[test]
+fn test1_3() {
+
+}
+
+#[test]
+fn test1_4() {
+
+}
+
+#[test]
+fn test1_5() {
+
+}
+
+#[test]
+fn test1_6() {
+
+}
+
+#[test]
+fn test1_7() {
+
+}
+
+#[test]
+fn test2_1() {
+
+}
+
+#[test]
+fn test2_2() {
+
+}
+
+#[test]
+fn test2_3() {
+
+}
+
+#[test]
+fn test3_1() {
+
+}
+
+#[test]
+fn test3_2() {
+
+}
+
+#[test]
+fn test3_3() {
+
+}
+
+#[test]
+fn test3_4() {
+
+}
+
+#[test]
+fn test3_5() {
+
+}
+
+#[test]
+fn test3_6() {
+
+}
+
+#[test]
+fn test3_7() {
+
+}
+
+#[test]
+fn test3_8() {
+
+}
+
+#[test]
+fn test3_9() {
+
+}
+
+#[test]
+fn test4_1() {
+
+}
+
+#[test]
+fn test4_2() {
+
+}
+
+#[test]
+fn test4_3() {
+
+}
+
+#[test]
+fn test4_4() {
+
+}
+
+#[test]
+fn test4_5() {
+
+}
+
+#[test]
+fn test5_1() {
+
+}
+
+#[test]
+fn test5_2() {
+
+}
+
+#[test]
+fn test5_3() {
+
+}
+
+#[test]
+fn test5_4() {
+
+}
+
+#[test]
+fn test5_5() {
+
+}
